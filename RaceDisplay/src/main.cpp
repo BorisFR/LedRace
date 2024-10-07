3132a8a2f4cbc3150f4fbe4cf64c85fb1242d1a7
Stéphane Fardoux <stephane.fardoux@yncrea.fr>
2024-10-07 15:54:21 +0200

    Correct param of the screen (color order, screen type, ...)

--- RaceDisplay/src/main.cpp
+++ RaceDisplay/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "./../../common/Common.h"
 
 /*
 DISPLAY
@@ -22,23 +23,64 @@ U2/RX: 16
 
 #include <TFT_eSPI.h> // Hardware-specific library
 #include <SPI.h>
+#include "Orbitron-Bold-80.h"
+#include "Roboto_Black_30.h"
 
 TFT_eSPI display = TFT_eSPI(); // Invoke custom library
+
+unsigned long last;
+byte countdownNumber;
+byte lapNumber;
+
+#define COUNTDOWN_POS_Y 30
+
+void showLap()
+{
+  display.setFreeFont(&Roboto_Black_30);
+  display.setTextDatum(TL_DATUM);
+  display.setTextColor(TFT_CYAN);
+  display.drawString("LAP " + String(lapNumber), 6, 6);
+}
 
 void setup()
 {
   Serial.begin(115200);
+  Serial2.begin(SERIAL_BAUD);
   display.init();
-  display.setRotation(2);
+  display.setRotation(0);
   display.fillScreen(TFT_BLACK);
-  display.setTextDatum(C_BASELINE);
-  // display.setFreeFont(&Roboto_14);
-  display.setTextColor(TFT_VIOLET);
-  display.drawString("ADIMAKER", TFT_WIDTH / 2, 15);
+
+  countdownNumber = 0;
+  lapNumber = 0;
+  showLap();
 }
 
 void loop()
 {
+  if (millis() - last > 700)
+  {
+    if (countdownNumber > 0)
+      countdownNumber--;
+    else
+      countdownNumber = 5;
+    display.fillScreen(TFT_BLACK);
+    if (countdownNumber == 0)
+    {
+      display.setFreeFont(&Orbitron_Bold_80);
+      display.setTextColor(TFT_GREEN);
+      display.setTextDatum(TC_DATUM);
+      display.drawString("GO", TFT_WIDTH / 2, COUNTDOWN_POS_Y);
+    }
+    else
+    {
+      display.setFreeFont(&Orbitron_Bold_80);
+      display.setTextColor(TFT_WHITE);
+      display.setTextDatum(TC_DATUM);
+      display.drawString(String(countdownNumber), TFT_WIDTH / 2, COUNTDOWN_POS_Y);
+    }
+    last = millis();
+    showLap();
+  }
   // display.setCursor(0, 0, 2);
   //  Set the font colour to be white with a black background, set text size multiplier to 1
   // display.setTextColor(TFT_WHITE, TFT_BLACK);
